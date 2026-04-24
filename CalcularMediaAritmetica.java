d06c43b0abda4905a2bf68d5c1ecf5ba61ef44fd
João Victor <va60234@gmail.com>
2026-04-24 20:17:05 -0300

    Add files via upload

---
+++ CalcularMediaAritmetica.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CalcularMediaAritmetica {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int contador = 0;
+        float soma = 0;
+
+        while (contador < 10) {
+            
+            System.out.println("Digite um número: ");
+            int numero = scan.nextInt();
+
+            soma += numero; //soma = soma + numero;
+            contador++;
+        }
+
+        float media = soma / 10;
+        
+        System.out.println("A média é " + media);
+
+    }
+}
